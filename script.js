55a9c21e63e07545c245a17984d3fed947bb4b37
HISCKLIF <105969356+HISCKLIF@users.noreply.github.com>
2022-07-01 21:25:29 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// let array = ['watermelon', 'pear', 10, 45, 50, 'apple', 'ananas'] ;
+
+
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[5]);
+// console.log(array[6]);
+
+
+
+
+
+// let array = [32, 10, 'hello', null, 'hello2', 50];
+
+// console.log(array[1]);
+// console.log(array[5]);
+
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+// for (let x = 5; x < 11; x++) {
+//     console.log(numbers[x]);
+// }
+
+
+
+
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'smith',
+//     age: 25,
+//     studentstatus: active
+// };
+
+//     console.log(user.studentstatus)
+
+
+
+
+
+
+
+
+
